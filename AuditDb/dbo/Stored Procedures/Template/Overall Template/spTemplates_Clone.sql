84a3fba3ce2073d2eee65cbf224dcb141d454ec5
Alex Sherman <alex.sherman@mavam.co.uk>
2024-06-25 15:16:39 +0100

    Created spTemplates_Clone, removed old temporary spTemplate_Clone (or whatever it was called, I forget)

---
+++ AuditDb/dbo/Stored Procedures/Template/Overall Template/spTemplates_Clone.sql
@@ -0,0 +1,33 @@
+﻿CREATE PROCEDURE [dbo].[spTemplates_Clone]
+    @ExistingTemplateId INT,
+    @NewTemplateName NVARCHAR(50),
+    @NewTemplateId INT OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    BEGIN TRY 
+        BEGIN TRANSACTION;
+        
+        INSERT INTO Templates (TemplateName, IsArchived)
+        VALUES (@NewTemplateName, 0);
+        
+        SET @NewTemplateId = SCOPE_IDENTITY();
+        
+        INSERT INTO Sections (TemplateId, Title, OrderInAudit, HasQuestions, IsArchived)
+        SELECT @NewTemplateId, Title, OrderInAudit, HasQuestions, 0
+        FROM Sections
+        WHERE TemplateId = @ExistingTemplateId;
+        
+        INSERT INTO Questions (SectionId, QuestionText, QuestionInfo, OrderInSection, IsArchived)
+        SELECT s.SectionId, q.QuestionText, q.QuestionInfo, q.OrderInSection, 0
+        FROM Questions q
+        INNER JOIN Sections s ON q.SectionId = s.SectionId
+        WHERE s.TemplateId = @ExistingTemplateId;
+        
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH
+END
