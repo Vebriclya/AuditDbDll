aa787234a2f12b7fef7e93250221d348526c7c70
Alex Sherman <alex.sherman@mavam.co.uk>
2024-06-25 14:40:37 +0100

    Added spQuestions_Archive

---
+++ AuditDb/dbo/Stored Procedures/Template/Questions/spQuestions_Archive.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[spQuestions_Archive]
+    @QuestionId INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    BEGIN TRY
+        BEGIN TRANSACTION;
+        UPDATE Questions
+        SET IsArchived = 1
+        WHERE QuestionId = @QuestionId;
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH
+END
