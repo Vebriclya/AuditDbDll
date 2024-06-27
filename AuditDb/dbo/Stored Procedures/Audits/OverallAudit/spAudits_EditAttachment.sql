8236a4f924e83202ac887cb5c5caceb793170803
Alex Sherman <alex.sherman@mavam.co.uk>
2024-06-27 14:10:08 +0100

    Added more stored procedures - Added sp for editing name of attachments - Added sp for adding attachment to a question - Added sp for adding attachment to a section

---
+++ AuditDb/dbo/Stored Procedures/Audits/OverallAudit/spAudits_EditAttachment.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[spAudits_EditAttachment]
+    @AttachmentId INT,
+    @AttachmentName NVARCHAR(255)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    BEGIN TRY 
+        BEGIN TRANSACTION;
+        UPDATE Attachments
+            SET AttachmentName = @AttachmentName
+            WHERE AttachmentId = @AttachmentId;
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH
+END
---
+++ AuditDb/dbo/Stored Procedures/Audits/Sections/spAuditSections_AddAttachment.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[spAudits_AddAttachment]
+    @SectionId INT,
+    @AttachmentName NVARCHAR(255),
+    @BlobUrl NVARCHAR(500),
+    @AttachmentId INT OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+        BEGIN TRANSACTION;
+        INSERT INTO Attachments (AuditId, SectionId, QuestionId, AttachmentName, BlobUrl, IsArchived)
+        VALUES (NULL, @SectionId, NULL, @AttachmentName, @BlobUrl, 0);
+
+        SET @AttachmentId = SCOPE_IDENTITY();
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH;
+END
