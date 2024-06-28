541e719823f965ac6fd8006ba3c7910bc081297d
Alex Sherman <alex.sherman@mavam.co.uk>
2024-06-28 14:27:08 +0100

    Added spSections_GetByTemplateId.sql!

---
+++ AuditDb/dbo/Stored Procedures/Template/Sections/spSections_GetByTemplateId.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[spSections_GetByTemplateId]
+    @TemplateId INT
+AS
+BEGIN 
+    SET NOCOUNT ON;
+    SELECT s.SectionId, 
+           s.TemplateId, 
+           s.Title, 
+           s.OrderInAudit, 
+           s.HasQuestions, 
+           s.IsArchived
+    FROM Sections s
+    WHERE s.TemplateId = @TemplateId
+    AND IsArchived = 0
+    ORDER BY s.OrderInAudit;
+END
+    
