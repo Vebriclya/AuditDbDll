517a2e371d16633608c2609a5ccb0933f57626fc
Alex Sherman <alex.sherman@mavam.co.uk>
2024-07-02 11:09:28 +0100

    Added spQuestions_GetByTemplate

---
+++ AuditDb/dbo/Stored Procedures/Template/Questions/spQuestions_GetByTemplate.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[spQuestions_GetByTemplate]
+@TemplateId INT
+AS
+BEGIN 
+    SET NOCOUNT ON;
+    SELECT q.QuestionId, 
+           q.SectionId, 
+           q.QuestionText,
+           q.QuestionInfo, 
+           q.OrderInSection, 
+           q.HasSelectBoxes, 
+           q.IsArchived
+    FROM Questions q
+    INNER JOIN Sections s ON q.SectionId = s.SectionId
+    WHERE TemplateId = @TemplateId
+      AND IsArchived = 0
+    ORDER BY s.OrderInAudit, q.OrderInSection;
+end
