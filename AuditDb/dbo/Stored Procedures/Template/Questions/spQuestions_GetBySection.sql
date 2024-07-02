b5e502095f3c5791bd622c1648650450398df099
Alex Sherman <alex.sherman@mavam.co.uk>
2024-07-02 10:56:18 +0100

    Added spQuestions_GetBySection!

---
+++ AuditDb/dbo/Stored Procedures/Template/Questions/spQuestions_GetBySection.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[spQuestions_GetBySectionId]
+    @SectionId INT
+AS
+    BEGIN
+        SET NOCOUNT ON;
+        SELECT q.QuestionId, 
+               q.SectionId, 
+               q.QuestionText, 
+               q.QuestionInfo, 
+               q.OrderInSection, 
+               q.HasSelectBoxes, 
+               q.IsArchived
+        FROM Questions q 
+        WHERE q.SectionId = @SectionId
+        AND q.IsArchived = 0
+        ORDER BY q.OrderInSection;
+    END
