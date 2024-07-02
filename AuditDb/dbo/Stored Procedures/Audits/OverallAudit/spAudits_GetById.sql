4a0d58bfe374ca4e497dc3f4fe49263dae8434d1
Alex Sherman <alex.sherman@mavam.co.uk>
2024-07-02 16:24:48 +0100

    Working on spAudits_GetById

---
+++ AuditDb/dbo/Stored Procedures/Audits/OverallAudit/spAudits_GetById.sql
@@ -0,0 +1,125 @@
+﻿CREATE PROCEDURE [dbo].[spAudits_GetById]
+    @AuditId INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    -- Get Audit details
+    SELECT a.AuditId,
+           a.TemplateId,
+           a.AuditDate,
+           a.AuditType,
+           a.IsCompleted,
+           a.IsArchived
+    FROM
+        Audits a
+    WHERE a.AuditId = @AuditId
+      AND a.IsArchived = 0
+    
+    -- Get Audit Sections
+    SELECT
+        s.SectionId,
+        s.AuditId,
+        s.Title,
+        s.OrderInAudit,
+        s.HasQuestions,
+        s.IsArchived
+    FROM
+        AuditSections s
+    WHERE s.AuditId = @AuditId
+      AND s.IsArchived = 0
+    ORDER BY s.OrderInAudit;
+    
+    -- Get Audit Questions
+    SELECT
+        q.QuestionId,
+        q.AuditSectionId,
+        q.QuestionText,
+        q.QuestionInfo,
+        q.OrderInSection,
+        q.HasSelectBoxes,
+        q.IsArchived
+    FROM
+        AuditQuestions q
+            INNER JOIN
+        AuditSections s on q.AuditSectionId = s.SectionId
+    WHERE s.AuditId = @AuditId 
+      AND s.IsArchived = 0
+      AND q.IsArchived = 0
+    ORDER BY
+        s.OrderInAudit,
+        q.OrderInSection;
+    
+    -- Get Section Responses
+    SELECT r.ResponseId,
+           r.AuditId,
+           NULL AS QuestionId,
+           r.SectionId, 
+           r.Score,
+           r.ResponseText,
+           r.IsArchived
+    FROM Responses r
+    WHERE r.AuditId = @AuditId
+      AND r.SectionId IS NOT NULL
+      AND r.QuestionId IS NULL
+      AND r.IsArchived = 0;
+    
+    -- Get Question Responses
+    SELECT r.ResponseId, 
+           r.AuditId, 
+           NULL AS SectionId,
+           r.QuestionId,
+           r.Score,
+           r.ResponseText,
+           r.IsArchived
+    FROM Responses r
+    WHERE r.AuditId = @AuditId
+      AND r.SectionId IS NULL
+      AND r.QuestionId IS NOT NULL
+      AND r.IsArchived = 0;
+    
+    -- Get Audit Attachments
+    SELECT att.AttachmentId, 
+           att.AuditId,
+           NULL AS SectionId,
+           NULL AS QuestionId,
+           att.AttachmentName, 
+           att.BlobUrl, 
+           att.IsArchived
+    FROM Attachments att
+    WHERE att.AuditId = @AuditId
+      AND att.SectionId IS NULL
+      AND att.QuestionId IS NULL
+      AND att.IsArchived = 0;
+    
+    -- Get Section Attachments
+    SELECT att.AttachmentId,
+           NULL AS AuditId,
+           att.SectionId,
+           NULL AS QuestionId,
+           att.AttachmentName,
+           att.BlobUrl,
+           att.IsArchived
+    FROM Attachments att
+    WHERE att.SectionId IN (SELECT SectionId FROM AuditSections WHERE AuditId = @AuditId)
+      AND att.AuditId IS NULL
+      AND att.QuestionId IS NULL
+      AND att.IsArchived = 0;
+    
+    -- Get Question Attachments
+    SELECT att.AttachmentId,
+           NULL AS AuditId,
+           NULL AS SectionId,
+           att.QuestionId,
+           att.AttachmentName,
+           att.BlobUrl,
+           att.IsArchived
+    FROM Attachments att
+    WHERE att.QuestionId IN (SELECT QuestionId FROM AuditQuestions WHERE AuditSectionId IN (SELECT SectionId FROM AuditSections WHERE AuditId = @AuditId))
+      AND att.AuditId IS NULL
+      AND att.SectionId IS NULL
+      AND att.IsArchived = 0;
+
+END
+    
+    
