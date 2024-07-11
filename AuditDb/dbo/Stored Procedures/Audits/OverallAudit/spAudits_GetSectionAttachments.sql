80b33bc2eb60208cf182a2bfa7526c3d87db6b79
Alex Sherman <alex.sherman@mavam.co.uk>
2024-07-11 20:49:43 +0100

    Added spAudits_GetSectionAttachments.sql

---
+++ AuditDb/dbo/Stored Procedures/Audits/OverallAudit/spAudits_GetSectionAttachments.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[spAudits_GetSectionAttachments]
+@SectionId INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    SELECT AttachmentId, 
+           AuditId, 
+           QuestionId, 
+           SectionId, 
+           AttachmentName, 
+           BlobUrl, 
+           IsArchived
+    FROM Attachments
+    WHERE SectionId = @SectionId
+      AND IsArchived = 0;
+END
