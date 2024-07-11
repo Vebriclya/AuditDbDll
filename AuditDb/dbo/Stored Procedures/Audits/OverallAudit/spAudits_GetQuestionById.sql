a66f9bc9e9e043baa083bc28ac796db162873c13
Alex Sherman <alex.sherman@mavam.co.uk>
2024-07-11 18:06:19 +0100

    Created spAudits_GetQuestionById.sql

---
+++ AuditDb/dbo/Stored Procedures/Audits/OverallAudit/spAudits_GetQuestionById.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[spAudits_GetQuestionById]
+    @QuestionId INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    SELECT q.QuestionId, 
+           q.AuditSectionId, 
+           q.QuestionText, 
+           q.QuestionInfo, 
+           q.OrderInSection, 
+           q.HasSelectBoxes, 
+           q.IsArchived
+    FROM AuditQuestions q
+    WHERE QuestionId = @QuestionId
+      AND IsArchived = 0;
+END
