37cc1bd0c39ce4fc02e9ada838638ba9ffbc7ec8
Alex Sherman <alex.sherman@mavam.co.uk>
2024-07-11 17:43:20 +0100

    Added spAudits_GetAllSections.sql

---
+++ AuditDb/dbo/Stored Procedures/Audits/OverallAudit/spAudits_GetAllSections.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[spAudits_GetAllSections]
+    @AuditId INT
+AS
+BEGIN 
+    SET NOCOUNT ON;
+    
+    SELECT a.SectionId, 
+           a.AuditId, 
+           a.Title, 
+           a.OrderInAudit, 
+           a.HasQuestions, 
+           a.IsArchived
+    FROM AuditSections a
+    WHERE AuditId = @AuditId
+      AND IsArchived = 0
+    ORDER BY OrderInAudit;
+    
+END
