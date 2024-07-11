a26ea23a41566a6a1a39e019ca3b632fc8b8c215
Alex Sherman <alex.sherman@mavam.co.uk>
2024-07-11 17:46:20 +0100

    Added spAudits_GetSectionById

---
+++ AuditDb/dbo/Stored Procedures/Audits/OverallAudit/spAudits_GetSectionById.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[spAudits_GetSectionById]
+    @SectionId INT
+AS
+BEGIN
+    SELECT a.SectionId, 
+           a.AuditId, 
+           a.Title, 
+           a.OrderInAudit, 
+           a.HasQuestions, 
+           a.IsArchived
+    FROM AuditSections a
+    WHERE a.SectionId = @SectionId
+END
